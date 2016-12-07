831fb080efeca42a84ed0b9567f134d3b726f05c
veivan <veivan@yandex.ru>
2016-12-07 11:22:30 +0300

    ReadINI and token got

--- src/main/java/com/ucoz/time/TwitterExample.java
+++ src/main/java/com/ucoz/time/TwitterExample.java
@@ -1,6 +1,8 @@
 package com.ucoz.time;
 
+import java.util.Properties;
 import java.util.Scanner;
+
 import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.apis.TwitterApi;
 import com.github.scribejava.core.model.OAuth1AccessToken;
@@ -9,19 +11,51 @@ import com.github.scribejava.core.model.OAuthRequest;
 import com.github.scribejava.core.model.Response;
 import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth10aService;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class TwitterExample {
 
     private static final String PROTECTED_RESOURCE_URL = "https://api.twitter.com/1.1/account/verify_credentials.json";
 
+    private static String CONSUMER_KEY;
+	private static String CONSUMER_SECRET;
+	private static String USER;
+	private static String USER_PASS;
+	
+	private static String oauth_token;
+	private static String oauth_token_secret;
+
     public TwitterExample() {
     }
 
+    private static void ReadINI() throws FileNotFoundException, IOException
+    {
+		Properties props = new Properties();
+		props.load(new FileInputStream(new File("example.ini")));
+		CONSUMER_KEY = props.getProperty("CONSUMER_KEY");
+		CONSUMER_SECRET = props.getProperty("CONSUMER_SECRET");
+		USER = props.getProperty("USER");
+		USER_PASS = props.getProperty("USER_PASS");
+		oauth_token = props.getProperty("oauth_token");
+		oauth_token_secret = props.getProperty("oauth_token_secret");
+    }
+    
+    public static void OperateWithAuthToken() throws IOException {
+    	
+    	ReadINI();
+    }
+    
     public static void twgo() throws IOException {
+    	
+    	ReadINI();
+    	
         final OAuth10aService service = new ServiceBuilder()
-                .apiKey("your key")
-                .apiSecret("your secret")
+                .apiKey(CONSUMER_KEY)
+                .apiSecret(CONSUMER_SECRET)
                 .build(TwitterApi.instance());
         final Scanner in = new Scanner(System.in);
 
@@ -60,5 +94,6 @@ public class TwitterExample {
 
         System.out.println();
         System.out.println("That's it man! Go and build something awesome with ScribeJava! :)");
+        in.close();
     }
 }
